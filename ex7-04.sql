9d88d0385833481021610cb0ceb0690e1cf843fd
Jean_ <127010423+Jean7340@users.noreply.github.com>
2025-08-17 18:01:43 -0400

    Delete ex7-04.sql

--- ex7-04.sql
+++
@@ -1,8 +0,0 @@
-SELECT product_name, discount_percent
-FROM products
-WHERE discount_percent NOT IN (
-    SELECT discount_percent
-    FROM products
-    GROUP BY discount_percent
-    HAVING count(discount_percent) > 1)
-ORDER BY product_name
