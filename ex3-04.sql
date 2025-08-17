59aa9feae5f52738e52142fef7fbc4c8b066992a
Jean_ <127010423+Jean7340@users.noreply.github.com>
2025-08-17 18:03:00 -0400

    Delete ex3-04.sql

--- ex3-04.sql
+++
@@ -1,6 +0,0 @@
-SELECT product_name, list_price, discount_percent,
-       ROUND(list_price * discount_percent * .01, 2) AS discount_amount,
-       list_price - ROUND(list_price * discount_percent * .01, 2) AS discount_price       
-FROM products
-ORDER BY discount_price DESC
-LIMIT 5;
